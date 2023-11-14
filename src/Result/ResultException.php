4df292fec43a6f29bc10a0ff2a42868a7e62bb87
Ghost <wahlolade@gmail.com>
2023-11-14 08:20:00 -0500

    add exception

---
+++ src/Result/ResultException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * ResultException
+ *
+ * @author Oskar Thornblad
+ */
+
+declare(strict_types=1);
+
+namespace Prewk\Result;
+
+use Exception;
+
+/**
+ * ResultException
+ */
+class ResultException extends Exception
+{
+}
